3e1d573de977482344b8d3b3db6a5d998fed4496
Valentin Titov <iosdevelop@yandex.ru>
2019-04-23 23:24:47 +0300

    Implemented task1 and task2 of lesson 3

---
+++ app/src/main/java/ru/gb/vtitov/popularlibraries/task1/LambdaObserveViewInterface.java
@@ -0,0 +1,8 @@
+package ru.gb.vtitov.popularlibraries.task1;
+
+public interface LambdaObserveViewInterface {
+
+	void initViewElements();
+	void printErrorMessage(String message);
+	void printMessage(String message);
+}
